cec9ed1c01c02ae4a9e3e280230a16182c95d8e9
BAEYJ92 <122344210+BAEYJ92@users.noreply.github.com>
2023-02-27 16:03:20 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,3 @@
-// const divBox = document.getElementsByClassName('box')[0];
-// console.log(divBox);
-
-// window.addEventListener('scroll', function(){
-//   let value = window.scrollY;
-//   console.log(value)
-
-//   if(value > 30) {
-//     divBox.style.animation = 'moving 10s linear forwards';
-//   }
-
-// });
-
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
@@ -25,7 +12,6 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
@@ -35,15 +21,6 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 1, // css에 지정한 슬라이더의 크기를 반영
   centeredSlides: true, // 슬라이드 패널을 항상 가운데로 반환
   speed: 500, // 슬라이더 이동 속도 지정
-  // effect: 'cards', // 이팩트 효과
-  // coverflowEffect: {
-  //   rotate: 50, // 회전각도
-  //   stretch: -200, // 슬라이드 간의 당김 정도
-  //   depth: 400, // 원근감 지정
-  //   modifier: 1, // 중첩 정도 지정
-  //   slideShadows: false //슬라이드 그림자 생성
-  // },
-
   autoplay: { // 자동 슬라이드
     delay: 1000,
     disableOnInteraction: true
@@ -53,7 +30,7 @@ const swiper = new Swiper('.swiper', {
 
 const btnStart = document.querySelector('li.btnStart');
 const btnStop = document.querySelector('li.btnStop');
-console.log(btnStart. btnStop);
+console.log(btnStart, btnStop);
 
 btnStart.addEventListener('click', function(){
   swiper.autoplay.start();
@@ -61,29 +38,6 @@ btnStart.addEventListener('click', function(){
 btnStop.addEventListener('click', function(){
   swiper.autoplay.stop();
 });
-
-// // 화면의 사이즈가 변하면 section 높이값을 고정
-// $('window').resize(function(){
-//   let ht = $(window).height();
-//   $('section').height(ht);
-// });
-
-//  // 메뉴 클릭 시 상하스크롤 하기
-// $('ul#gnb li').click(function(e){
-    
-//   // 고유의 기능을 막아준다.(a 태그)
-//   e.preventDefault();
-
-//   // 화면의 높이값
-//   let ht = $(window).height();
-//   // 클릭한 요소의 인덱스 값
-//   let i = $(this).index();
-//   // 인덱스 값 * 화면의 높이 값
-//   let nowTop = i * ht;
-//   // 해당 위치로 스크롤 이동
-//   $('html, body').stop().animate({scrollTop : nowTop}, 1400);
-
-// });
 
   // 메뉴 클릭 시 상하스크롤 하기
   $('ul#gnb li').click(function(e){
